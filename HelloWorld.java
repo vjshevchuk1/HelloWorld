9df7df3f2cf4090636f9e48ecffd17ccf51712c5
vjshevchuk1 <147752648+vjshevchuk1@users.noreply.github.com>
2024-01-21 19:28:50 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-public class Main {
-   public static void main(String[] args) {
-     System.out.println("Hello world!");
-   }
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("Меня зовут Виталий");
+        System.out.println("Мне 18 лет");
+        System.out.println("Я из Тернополя");
+    }
 }
